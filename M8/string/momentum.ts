fb0c5197fdf261c4b32a8fc77023fac691876acf
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-02-20 14:50:36 +0800

    Update momentum.ts

--- M8/string/momentum.ts
+++ M8/string/momentum.ts
@@ -1,12 +1,12 @@
 void function Momentum(
     m1: number, // 初始粒子1的质量
-    v1: number, // 初始粒子1的速度
+    v1: string, // 初始粒子1的速度
     m2: number, // 初始粒子2的质量
-    v2: number, // 初始粒子2的速度
+    v2: string, // 初始粒子2的速度
     m3: number, // 最终粒子3的质量
-    v3: number, // 最终粒子3的速度
+    v3: string, // 最终粒子3的速度
     m4: number, // 最终粒子4的质量
-    v4: number  // 最终粒子4的速度
+    v4: string  // 最终粒子4的速度
 ):boolean{
     let p1 = m1 * v1
     let p2 = m2 * v2
